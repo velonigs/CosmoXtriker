bfd3d2aa224c95d4e3b8adbdee014c0eec6109d2
roberto365 <milone365@gmail.com>
2019-07-10 09:38:31 +0900

    RandomMovementとFollowPlayerスクリプトの追加

---
+++ Assets/Resources/Enemy/FollowPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private float followRange = 500f;
+   
+    [SerializeField]
+    private float attackrange = 100f;
+    private PlayerController player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //プレーヤーを探す機能
+        player = FindObjectOfType<PlayerController>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //プレーヤーと敵の距離
+        var distanceToPlayer = (transform.position-player.transform.position).sqrMagnitude;
+      
+        //レンジを作って、その中でエネミーはプレーヤの場へ向かう
+        if (distanceToPlayer <= followRange&&distanceToPlayer>attackrange)
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, 0.2f);
+        }
+        else
+        {　　　//真っ直ぐへ移動
+            transform.Translate(transform.position.x, transform.position.y, -0.2f);
+        }
+    }
+}
---
+++ Assets/Resources/Enemy/RandomMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour
+{    //最小と最大の値を決める
+    [SerializeField] private float minValue;
+    [SerializeField] private float MaxValue;
+    //プレーヤーへ向かう
+    [SerializeField] private float moveZ=-0.2f;
+    float movex;
+    float movey; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ｘとｙ座標をランダムに決める
+        movex = Random.Range(minValue, MaxValue);
+        movey = Random.Range(minValue, MaxValue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(movex, movey, moveZ);
+    }
+}
