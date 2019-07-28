c3dbffa2c301f256798b2b70751e3d8b539cc4d2
Nerlite <ctrlaltenter0@gmail.com>
2019-07-28 18:50:34 +0900

    倉庫内移動仮

--- Assets/EventDirector.cs
+++ Assets/EventDirector.cs
@@ -2,17 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventDirector : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class EventDirector : MonoBehaviour {
+
+    [SerializeField]
+    private List<Transform> _transLists = new List<Transform>();
+
+    [SerializeField]
+    private float _speed = 1.0f;
+
+    private int _selectListNum = 0;
+
+    private Vector3 _startPos;
+    private Vector3 _endPos;
+    private Quaternion _startRotate;
+    private Quaternion _endRotate;
+    private float _time;
+    private float _deltaTime;
+    private float _angle = 0;
+    
+    void Start() {
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void StartAction() {
+        // _selectListNum = 0;
+        _startPos = transform.localPosition;
+        _endPos = _transLists[_selectListNum].localPosition;
+        _startRotate = transform.rotation;
+        _endRotate = _transLists[_selectListNum].rotation;
+        _deltaTime = 1.0f / 120.0f;
+        StartCoroutine(StartActionCoroutine());
+    }
+
+    IEnumerator StartActionCoroutine() {
+        while (_time < 1.0f) {
+            _time += _deltaTime;
+
+            if (_time >= 1.0f) {
+                _time = 1.0f;
+            }
+
+            var x = Mathf.Lerp(_startPos.x, _endPos.x, _time);
+            var y = Mathf.Lerp(_startPos.y, _endPos.y, _time);
+            var z = Mathf.Lerp(_startPos.z, _endPos.z, _time);
+            transform.localPosition = new Vector3(x, y, z);
+
+            transform.rotation = Quaternion.Lerp(_startRotate, _endRotate, _time);
+            yield return null;
+        }
+
+        _angle = 0;
+
+        if (_selectListNum == 0) {
+            yield return null;
+        }
+
+        _selectListNum++;
+
+        if (_selectListNum < _transLists.Count) {
+            _startPos = transform.localPosition;
+            _endPos = _transLists[_selectListNum].localPosition;
+            _time = 0;
+            StartCoroutine(StartActionCoroutine());
+        }
     }
 }
--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class GameController : MonoBehaviour {
+
+    [SerializeField]
+    private VehicleController _vehicle;
+
+    [SerializeField]
+    private GameObject _tutorialCanvas;
+
+    private bool _tutorialCanvasFlg = false;
+    
+    void Start () {
+        _tutorialCanvasFlg = _tutorialCanvas.activeSelf;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    
+    void Update () {
+        if (_tutorialCanvas.activeSelf == false && _tutorialCanvasFlg != _tutorialCanvas.activeSelf) {
+            _vehicle.IsAction = VehicleController.VehicleControlStatus.Action;
+        }
+
+        _tutorialCanvasFlg = _tutorialCanvas.activeSelf;
     }
 }
