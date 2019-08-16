c2f8d329a5e5d3867cfe85dd3ad646d2fe1d3ca4
Nerlite <ctrlaltenter0@gmail.com>
2019-08-16 18:20:56 +0900

    タイトル文字の点滅、Fadeの調整。

---
+++ Assets/Resources/UI/BlinkText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkText : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _text;
+
+    private float _nextTime;
+    public float _interval = 0.8f;
+    
+    void Start () {
+        _nextTime = Time.time;
+    }
+
+    
+    void Update () {
+        if (Time.time > _nextTime) {
+            float alpha = _text.GetComponent<CanvasRenderer>().GetAlpha();
+            if (alpha == 1.0f) {
+                _text.GetComponent<CanvasRenderer>().SetAlpha(0.0f);
+            } else {
+                _text.GetComponent<CanvasRenderer>().SetAlpha(1.0f);
+            }
+            _nextTime += _interval;
+        }
+    }
+}
