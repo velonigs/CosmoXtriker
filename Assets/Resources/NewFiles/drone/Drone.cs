884fa89b5c832845135b14816732033f8cfed6d8
roberto <milone365@gmail.com>
2019-08-19 16:52:34 +0900

    drone 完成

---
+++ Assets/Resources/NewFiles/drone/Drone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drone : CorvetteMovement
+{
+
+    [SerializeField]
+    GameObject bullet;
+    [SerializeField] float fireDelay=2f;
+    float fireCounter;
+    [SerializeField] Transform spawnpoint;
+
+    private void Start()
+    {
+        Initialize();
+        fireCounter = fireDelay;
+    }
+    private void Update()
+    {
+        Move();
+        fireCounter -= Time.deltaTime;
+        if (fireCounter <= 0)
+        {
+            fireCounter = fireDelay;
+            Instantiate(bullet, spawnpoint.position, spawnpoint.rotation);
+        }
+    }
+
+}
---
+++ Assets/Resources/NewFiles/turret/Gatling.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gatling : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform spawnpoint;
+    [SerializeField]
+    Transform rotationPoint;
+    [SerializeField]
+    Transform Torret;
+    [SerializeField]
+    float velocity = 10;
+   public bool fire;
+    Transform player;
+    [SerializeField]
+    float offset;
+    [SerializeField]
+    float fireDelay=0.25f;
+    float fireCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = PlayerController.instance.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player!=null){
+            Torret.LookAt(player, Vector3.up);
+            Torret.rotation = Quaternion.Euler(0, Torret.eulerAngles.y+offset, 0);
+        }
+        if (fire)
+        {
+            rotationPoint.Rotate(velocity, 0, 0);
+            fireCounter *= Time.deltaTime;
+            if (fireCounter <= 0)
+            {
+                fireCounter = fireDelay;
+                Instantiate(bullet, spawnpoint.position, spawnpoint.rotation);
+            }
+           
+        }
+       
+    }
+}
