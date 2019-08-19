ab8fbd4584b3bd59935290a21e5460faf7f0043f
roberto <milone365@gmail.com>
2019-08-19 15:43:52 +0900

    missile Attack

--- Assets/Resources/Enemy/DestroyerHead.cs
+++ Assets/Resources/Enemy/DestroyerHead.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class DestroyerHead :_tmpEnemys
 {
+    [SerializeField] float chageAttackTimer = 10f;
+    float changeAttackCounte;
     [SerializeField] float fireRate = 2f;
     public static DestroyerHead instance;
+    [SerializeField]
+    bomberMissile missiles;
+    [SerializeField]
+    Transform[] missileSpawnPoints;
     [SerializeField]
     DestroyerCannon[] cannons;
     int currentAttack;
     float fireCounter;
+    string attackType = "";
+    bool missilesAttack { get { return attackType == "misile"; } } 
+        
+    bool bulletAttack { get { return attackType == "bullet"; } }
     private void Awake()
     {
         instance = this;
+        attackType = "bullet";
     }
 
     private void Start()
@@ -22,22 +33,42 @@ public class DestroyerHead :_tmpEnemys
     }
     private void Update()
     {
+        changeAttackCounte -= Time.deltaTime;
+        if (changeAttackCounte <= 0)
+        {
+            changeAttackCounte = chageAttackTimer;
+            if (attackType == "missile") { attackType = "bullet"; }
+           else if (attackType == "bullet") { attackType = "missile"; }
+            
+            
+        }
         fireCounter -= Time.deltaTime;
         if (fireCounter <= 0)
         {
             fireCounter = fireRate;
-            
-            for(; ; )
+            if (bulletAttack)
             {
-                if (cannons[currentAttack] != null)
+                for (; ; )
                 {
-                    cannons[currentAttack].fire();
-                    currentAttack++;
-                    if (currentAttack >= cannons.Length) { currentAttack = 0; }
-                    break;
+                    if (cannons[currentAttack] != null)
+                    {
+                        cannons[currentAttack].fire();
+                        currentAttack++;
+                        if (currentAttack >= cannons.Length) { currentAttack = 0; }
+                        break;
+                    }
                 }
             }
+            if (missilesAttack)
+            {
+
+                Instantiate(missiles, missileSpawnPoints[currentAttack].position, missileSpawnPoints[currentAttack].rotation);
+                currentAttack++;
+                if (currentAttack >= missileSpawnPoints.Length) { currentAttack = 0; }
+
+            }
         }
+        
     }
 
 }
