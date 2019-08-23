d5c32701350e0003bc22c8034090b9367cc83c7b
roberto365 <milone365@gmail.com>
2019-08-23 15:00:09 +0900

    ミニマップサンプルを追加

---
+++ Assets/Resources/NewFiles/Minimap/mapCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapCamera : MonoBehaviour
+{
+    Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerController.instance != null)
+        {
+            target = PlayerController.instance.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+            transform.rotation = Quaternion.Euler(90, target.eulerAngles.y, 0);
+        }
+    }
+}
