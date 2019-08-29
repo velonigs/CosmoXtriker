424696dafd93570e23e07a82cb421d505c971eca
Nerlite <ctrlaltenter0@gmail.com>
2019-08-30 03:38:25 +0900

    体力連動実装

---
+++ Assets/HorogramColorChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorogramColorChanger : HealthManager {
+
+    void Start () {
+
+    }
+
+    void Update () {
+        if (health > startingHealth / 2) {
+            GetComponent<Renderer>().material.color = new Color32(0, 255, 0, 100);
+        } else if (health > startingHealth / 5) {
+            GetComponent<Renderer>().material.color = new Color32(255, 255, 0, 100);
+        } else if (health < startingHealth / 5) {
+            GetComponent<Renderer>().material.color = new Color32(255, 0, 0, 100);
+        }
+    }
+}
--- Assets/Resources/Player/HealthManager.cs
+++ Assets/Resources/Player/HealthManager.cs
@@ -6,29 +6,17 @@ public class HealthManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject gameover;
-
-    // [SerializeField]
-    // private GameObject _strikerHologram;
-
-    public float health;
+    public float health = 500;
     public float startingHealth = 500;
 
     void Start()
     {
-        health = startingHealth;
+        
     }
 
-    // TODO:体力の残量によって自機ホログラムの色を変更されるようにする
     void Update()
     {
-        if (health > startingHealth / 2)
-        {
-
-        }
-        else
-        {
-
-        }
+        
     }
 
     public void Takedamage(float damageToTake)
