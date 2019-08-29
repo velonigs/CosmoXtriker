c0a6b4b0cd4ac1b061074196dd170d0db74ead2a
roberto365 <milone365@gmail.com>
2019-08-29 20:15:09 +0900

    意外とDestroyerは出来ている、あとはミッサイルのスポーンポジションを直すが必要です。

---
+++ Assets/DestroyerCannonCoordinator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyerCannonCoordinator : MonoBehaviour
+{
+    public Transform[] cannon;
+    protected float RotationSpeed = 10;
+    protected Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerController.instance != null)
+        {
+            player = PlayerController.instance.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == null) return;
+        if (player.position.y >= cannon[0].position.y)
+        {
+            for (int i = 0; i < cannon.Length; i++)
+            {
+
+                Quaternion targetRot = Quaternion.LookRotation(player.position - cannon[i].position);
+                cannon[i].rotation = Quaternion.Lerp(cannon[i].rotation, targetRot, RotationSpeed * Time.deltaTime);
+
+            }
+        }
+      
+    }
+}
