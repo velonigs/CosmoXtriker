2f192524941b4cf3f03eef3ffdeee2b75d075799
Velonigs <a>
2019-08-30 13:02:31 +0900

    a

--- Assets/Resources/Enemy/Wave3.cs
+++ Assets/Resources/Enemy/Wave3.cs
@@ -6,16 +6,16 @@ public class Wave3 : MonoBehaviour
 {
     // Debri収納
     GameObject Debri;
-    // Corvette Wave2時
-    [SerializeField]
+    // Corvette Wave3時
+    /* [SerializeField]
     GameObject Corvette3;
     // Corvette Wave6時
     [SerializeField]
-    GameObject Corvette6;
+    GameObject Corvette6;*/
     // Waveプレハブを収納する
     public GameObject[] waves;
     //現在のWave
-    private int currentWave = 0;
+    private int currentWave;
 
 
 
@@ -32,7 +32,7 @@ public class Wave3 : MonoBehaviour
         {
 
         //Waveを作成する,プレイヤーのポジションｘとｙと同じ講座
-        GameObject wave = (GameObject)Instantiate(waves[currentWave], new Vector3(PlayerController.instance.transform.position.x,PlayerController.instance.transform.position.y,transform.position.z)
+        GameObject wave = (GameObject)Instantiate(waves[currentWave], new Vector3(PlayerController.instance.transform.position.x,PlayerController.instance.transform.position.y,waves[currentWave].transform.position.z)
             , Quaternion.identity);
 
             //WaveをEmitterの子要素にする
@@ -48,15 +48,14 @@ public class Wave3 : MonoBehaviour
             {
                 this.Debri.GetComponent<DebrisPop>().enabled = false;
             }
-            //Waveが3になったらCorvette Event Proをオンに
-            if (currentWave == 2)
+            /* if (currentWave == 2)
             {
                 Corvette3.SetActive(true);
             }
             if (currentWave == 5)
             {
                 Corvette6.SetActive(true);
-            }
+            }*/
             //Waveの子要素のEnemyがすべて消去されるまで待機する
             while (wave.activeSelf == true)
             {
