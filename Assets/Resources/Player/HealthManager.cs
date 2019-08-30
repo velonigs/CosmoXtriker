eeff33775abec21fee93b7155197aa351fde2040
mametaro117 <16c1020003ik@edu.tech.ac.jp>
2019-08-30 17:20:39 +0900

    ホログラムの色と自機の体力連動実装

--- Assets/Resources/Player/HealthManager.cs
+++ Assets/Resources/Player/HealthManager.cs
@@ -6,8 +6,13 @@ public class HealthManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject gameover;
+
+    [SerializeField]
+    private HorogramColorChanger hcChanger;
+
     public float health = 500;
     public float startingHealth = 500;
+
 
     void Start()
     {
@@ -29,6 +34,18 @@ public class HealthManager : MonoBehaviour
             if (gameover != null) 
             gameover.SetActive(true);
             gameObject.SetActive(false);
+        }
+        else if (health > startingHealth / 2) 
+        {
+            hcChanger.ColorChange_Green();
+        }
+        else if (health <= startingHealth / 2 && health > startingHealth / 5) 
+        {
+            hcChanger.ColorChange_Yellow();
+        }
+        else if (health <= startingHealth / 5)
+        {
+            hcChanger.ColorChange_Red();
         }
     }
 }
