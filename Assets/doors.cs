9ff70d8ca9e489602f86bfd34eac75d46b106e2e
tokyotech-hosyu <tokyotech-hosyu@4900.co.jp>
2019-08-30 17:15:08 +0900

    ラストステージのドア

---
+++ Assets/doors.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doors : MonoBehaviour
+{
+    Animator _anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = this.gameObject.GetComponent<Animator>();
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            _anim.enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
