26473f4d48b77b286d981016f4bc143572bb2612
mametaro117 <16c1020003ik@edu.tech.ac.jp>
2019-09-05 18:50:31 +0900

    進歩なし

--- Assets/TutorialCore.cs
+++ Assets/TutorialCore.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class TutorialCore : MonoBehaviour {
 
     [SerializeField]
+    private GameObject _successText;
+
+    [SerializeField]
+    private GameObject _striker;
+
+    [SerializeField]
     private GameObject _doingTutorial;
 
     [SerializeField]
@@ -15,6 +21,8 @@ public class TutorialCore : MonoBehaviour {
 
     private AudioSource _voice1;
     private AudioSource _voice2;
+
+    private bool _EndMoveTutorial = false;
 
     public enum TutorialWaves {
         Move,
@@ -28,7 +36,6 @@ public class TutorialCore : MonoBehaviour {
         _voice1 = gameObject.GetComponent<AudioSource>();
         _voice2 = gameObject.GetComponent<AudioSource>();
         _voice1.clip = _tutorialDescription;
-        _voice2.clip = _moveTutorial;
         _voice1.Play();
     }
 
@@ -36,7 +43,24 @@ public class TutorialCore : MonoBehaviour {
     void Update () {
         if (_tutorialButton._beginTutorial == true) {
             _voice1.Stop();
+            _voice2.clip = _moveTutorial;
             _voice2.Play();
+            _striker.GetComponent<PlayerController>().enabled = true;
         }
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (_striker.gameObject.tag == "EndMoveTutorial") {
+            _tutorialButton._beginTutorial = true;
+            StartCoroutine(SuccessText());
+            _EndMoveTutorial = true;
+        }
+    }
+
+    IEnumerator SuccessText() {
+        _successText.SetActive(true);
+        yield return new WaitForSeconds(3.0f);
+        _successText.SetActive(false);
     }
 }
