ee4eadf9e03923813c6de67311c3adb24219ea55
Jason <tokyotech-hosyu@4900.co.jp>
2019-09-06 11:38:25 +0900

    sceneLoader

---
+++ Assets/Resources/roberto/scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    public string sceneToLoad;
+    public string mainMenu;
+    public void loadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void goToMainMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
