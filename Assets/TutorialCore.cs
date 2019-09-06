f5596b8345261e798555023d82fce4b71f423019
mametaro117 <16c1020003ik@edu.tech.ac.jp>
2019-09-06 15:56:53 +0900

    射撃チュートリアル途中

--- Assets/TutorialCore.cs
+++ Assets/TutorialCore.cs
@@ -19,30 +19,42 @@ public class TutorialCore : MonoBehaviour {
     [SerializeField]
     private TutorialButton _tutorialButton;
 
+    [SerializeField]
+    private GameObject _spawnController;
+
+    [SerializeField]
+    private GameObject _enemySpawn;
+
+    [SerializeField]
+    private GameObject _defaultPosition;
+
     public AudioClip _tutorialDescriptionVoice;
     public AudioClip _moveTutorialVoice;
+    public AudioClip _weaponCannonVoice;
+    public AudioClip _weaponMissileVoice;
+    public AudioClip _evadeTutorialVoice;
+    public AudioClip _flareTutorialVoice;
 
     private AudioSource _voice1;
     private AudioSource _voice2;
+    private AudioSource _voice3;
+    private AudioSource _voice4;
+    private AudioSource _voice5;
+    private AudioSource _voice6;
 
-    private bool _EndMoveTutorial = false;
-
-    public enum TutorialWaves {
-        Move,
-        Battle,
-        Evade,
-        Flare
-    }
-    public TutorialWaves _tutorialWaves;
+    private bool _spawnTutorialEnemy = false;
     
     void Start () {
         _voice1 = gameObject.GetComponent<AudioSource>();
         _voice2 = gameObject.GetComponent<AudioSource>();
+        _voice3 = gameObject.GetComponent<AudioSource>();
+        _voice4 = gameObject.GetComponent<AudioSource>();
+        _voice5 = gameObject.GetComponent<AudioSource>();
+        _voice6 = gameObject.GetComponent<AudioSource>();
         _voice1.clip = _tutorialDescriptionVoice;
         _voice1.Play();
     }
 
-    // enum TutorialWavesをForで回してチュートリアルのフローを作る
     void Update () {
         if (_tutorialButton._beginTutorial == true) {
             _voice1.Stop();
@@ -57,12 +69,29 @@ public class TutorialCore : MonoBehaviour {
             }
         }
 
+        if (_spawnTutorialEnemy) {
+            _enemySpawn = Instantiate(_spawnController, _enemySpawn.transform.position, Quaternion.identity);
+            _spawnTutorialEnemy = false;
+        }
     }
+
+    // 成功表示が出たら初期位置まで自機を誘導してその間は操作不能にする
+
+    // 全ての敵を倒したら次のチュートリアルに移行する
+    // 敵一体ずつ配列型に格納して撃破判定はバレットのタグが当たったらにする
 
     public IEnumerator SuccessText() {
         _successText.SetActive(true);
         yield return new WaitForSeconds(2.0f);
         _successText.SetActive(false);
+        yield return new WaitForSeconds(2.0f);
+        StartCoroutine(CannonTutorial());
     }
 
+    public IEnumerator CannonTutorial() {
+        _spawnTutorialEnemy = true;
+        _voice3.clip = _weaponCannonVoice;
+        _voice3.Play();
+        yield return null;
+    }
 }
