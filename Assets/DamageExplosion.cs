7c069b8b9d976a60242f78d706d92a4de66b0e49
Jason <tokyotech-hosyu@4900.co.jp>
2019-09-06 11:47:58 +0900

    damage explosion

---
+++ Assets/DamageExplosion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageExplosion : MonoBehaviour
+{
+    int damage = 0;
+    private void Start()
+    {
+        damage = UnityEngine.Random.Range(5, 15); 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player"){
+            other.GetComponent<HealthManager>().Takedamage(damage);
+            }
+    }
+}
