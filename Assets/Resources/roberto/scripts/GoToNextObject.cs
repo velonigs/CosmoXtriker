53570680a7fffc31f83df1834179b1ed0a9a36a6
roberto365 <milone365@gmail.com>
2019-09-07 07:56:36 +0900

    Sentinel boostとrotation設定の改善

---
+++ Assets/Resources/roberto/scripts/GoToNextObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToNextObject : MonoBehaviour
+{
+    [SerializeField]GameObject[] array;
+    int current;
+
+    public void activeNext(int obj)
+    {
+        array[obj].SetActive(true);
+    }
+
+
+}
