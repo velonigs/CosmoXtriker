598ec1554224115d924764337fefb37e8f8b368d
mametaro117 <16c1020003ik@edu.tech.ac.jp>
2019-09-06 17:43:33 +0900

    射撃チュートリアル途中

--- Assets/TutorialCore.cs
+++ Assets/TutorialCore.cs
@@ -26,7 +26,7 @@ public class TutorialCore : MonoBehaviour {
     private GameObject _enemySpawn;
 
     [SerializeField]
-    private GameObject _defaultPosition;
+    private MoveDefault _moveDefault;
 
     public AudioClip _tutorialDescriptionVoice;
     public AudioClip _moveTutorialVoice;
@@ -43,6 +43,14 @@ public class TutorialCore : MonoBehaviour {
     private AudioSource _voice6;
 
     private bool _spawnTutorialEnemy = false;
+    private bool _tutorialDirection = false;
+    private bool _returnDefault = false;
+    public bool ReturnDefault {
+        get {
+            return _returnDefault;
+        }
+    }
+    
     
     void Start () {
         _voice1 = gameObject.GetComponent<AudioSource>();
@@ -65,14 +73,19 @@ public class TutorialCore : MonoBehaviour {
 
         for (int i = 0; i < _moveTutorial.Length; i++) {
             if (_moveTutorial[i].BattleTutorialFlg) {
-                StartCoroutine(SuccessText());
+                StartCoroutine(CannonTutorial());
             }
         }
 
-        if (_spawnTutorialEnemy) {
-            _enemySpawn = Instantiate(_spawnController, _enemySpawn.transform.position, Quaternion.identity);
-            _spawnTutorialEnemy = false;
+        if (_moveDefault.EndReturnMove) {
+            _spawnTutorialEnemy = true;
+            _voice3.clip = _weaponCannonVoice;
+            _voice3.Play();
         }
+
+        // if () {
+        //     StartCoroutine(CannonTutorial());
+        // }
     }
 
     // 成功表示が出たら初期位置まで自機を誘導してその間は操作不能にする
@@ -80,18 +93,25 @@ public class TutorialCore : MonoBehaviour {
     // 全ての敵を倒したら次のチュートリアルに移行する
     // 敵一体ずつ配列型に格納して撃破判定はバレットのタグが当たったらにする
 
-    public IEnumerator SuccessText() {
+    public IEnumerator CannonTutorial() {
         _successText.SetActive(true);
+        _returnDefault = true;
         yield return new WaitForSeconds(2.0f);
         _successText.SetActive(false);
-        yield return new WaitForSeconds(2.0f);
-        StartCoroutine(CannonTutorial());
+
+        if (_spawnTutorialEnemy) {
+            //_enemySpawn = Instantiate(_spawnController, _enemySpawn.transform.position, Quaternion.identity);
+            _spawnTutorialEnemy = false;
+        }
+        
     }
 
-    public IEnumerator CannonTutorial() {
-        _spawnTutorialEnemy = true;
-        _voice3.clip = _weaponCannonVoice;
-        _voice3.Play();
+    public IEnumerator MissileTutorial() {
+        
         yield return null;
     }
+
+    // public IEnumerator SuccessText() {
+
+    // }
 }
