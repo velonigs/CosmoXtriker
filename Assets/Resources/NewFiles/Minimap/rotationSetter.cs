a78ac637a3535cd154af653ac3a50d7b57cfddcf
roberto365 <milone365@gmail.com>
2019-09-06 23:59:24 +0900

    minimapTest

---
+++ Assets/Resources/NewFiles/Minimap/rotationSetter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationSetter : MonoBehaviour
+{
+    Quaternion startRot = new Quaternion();
+    // Start is called before the first frame update
+    void Start()
+    {
+        startRot = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.rotation != startRot)
+        {
+            transform.rotation = startRot;
+        }
+    }
+}
