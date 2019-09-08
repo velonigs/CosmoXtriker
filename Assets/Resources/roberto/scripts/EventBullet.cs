cce5b9be8de027ec9b8725d3a1edd59108a91d72
milone365 <a>
2019-09-08 16:17:17 +0900

    bulled

---
+++ Assets/Resources/roberto/scripts/EventBullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventBullet : MonoBehaviour
+{
+    public int damage = 5;
+    [SerializeField]
+    protected float speedMultipler = 1f;
+    [SerializeField]
+    protected float bulletSpeed = 0.4f, rotateSpeed = 1;
+
+
+    Vector3 _target;
+    Vector3 direction;
+    private void Start()
+    {
+        Destroy(gameObject, 3);
+        
+    }
+    public void givetarget(Vector3 tg)
+    {
+        _target = tg;
+        direction = _target- transform.position;
+        
+    }
+
+    private void Update()
+    {
+        transform.position += direction * Time.deltaTime*bulletSpeed*speedMultipler;
+    }
+}
+
+    
