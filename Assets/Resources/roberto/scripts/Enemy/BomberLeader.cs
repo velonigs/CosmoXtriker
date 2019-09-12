4b8b06d54448a5276f79a1ff33a932dc55d60c28
roberto365 <milone365@gmail.com>
2019-09-12 23:54:11 +0900

    fighters and Bombers multipleMovements

--- Assets/Resources/roberto/scripts/Enemy/BomberLeader.cs
+++ Assets/Resources/roberto/scripts/Enemy/BomberLeader.cs
@@ -20,26 +20,6 @@ public class BomberLeader : _tmpEnemys
 
     public override void Movement()
     {
-       /* if (hit)
-        {
-            if (hitTimer > 0)
-            {
-
-                foreach (var r in rends)
-                {
-                    r.enabled = false;
-                }
-                hitTimer -= Time.deltaTime;
-                if (hitTimer <= 0)
-                {
-                    hit = false;
-                    foreach (var r in rends)
-                    {
-                        r.enabled = true;
-                    }
-                }
-            }
-        }*/
        
     }
 
@@ -50,14 +30,17 @@ public class BomberLeader : _tmpEnemys
         {
             ushort _dmg = collision.gameObject.GetComponent<_tmpBullet>().Damage;
             HP -= _dmg;
-           // hitTimer = 0.15f;
-           // hit = true;
+           
             if (HP <= 0)
             {
 
                 allEnemyes = FindObjectsOfType<Bomber>();
                 for (int i = 0; i < allEnemyes.Length; i++)
                 {
+                    if (allEnemyes[i].randomMove)
+                    {
+                        allEnemyes[i].randomMove = false;
+                    }
                     allEnemyes[i].randomMove = true;
                 }
                 Instantiate(Explode, transform.position, transform.rotation); //現在位置にエフェクト生成
